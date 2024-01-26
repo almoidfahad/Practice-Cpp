983baf9fe59bb01f5b4b68170f6b0274ad5081bb
Al Moid Fahad <almoidfahad@gmail.com>
2024-01-27 05:45:29 +0600

    pattern type - 3/1

---
+++ pattern_type-3/pattern_1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std; 
+int main(){
+    int n, row , col;
+    cout<<"Enter Number: ";
+    cin>> n;
+    
+    for(row = 1 ; row <= n; row++){
+        for(col=1; col<=row; col++){
+            cout<<" "<<col;
+        }
+        cout<<endl;
+    }
+    for(row = n-1; row >= 1; row--){
+        for(col=1; col<=row; col++){
+            cout<<" "<<col;
+        }
+        cout<<endl;
+    }
+}
