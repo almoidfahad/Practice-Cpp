8c7e18dc48aae7c5506513159ed11b07771fa61e
Al Moid Fahad <almoidfahad@gmail.com>
2024-01-27 08:49:21 +0600

    pattern 3/3

---
+++ pattern_type-3/pattern_3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n, row, col;
+    cout<<"Enter any Number: " ;
+    cin>> n;
+    
+    for(row = 1 ; row<=n; row++){
+        for(col= 1 ; col<=row; col++){
+            cout<<" "<<char(col+64);
+        }
+        cout<<endl;
+    }
+    for(row = n-1; row>=1; row--){
+        for(col=1; col<=row; col++){
+            cout<<" "<< char(col+64);
+        }
+        cout<<endl;
+    }
+    
+}
