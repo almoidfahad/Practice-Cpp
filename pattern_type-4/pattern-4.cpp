3af2cb43724be1381454f3a42e4444f8e9a7b1f0
Al Moid Fahad <almoidfahad@gmail.com>
2024-01-28 17:45:22 +0600

    pattern 4/1,2,3,4,5

---
+++ pattern_type-4/pattern-4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, row, col;
+    cout<<"Enter any Number: ";
+    cin>> n;
+    
+    for (row = 1; row <= n; row++){
+        for (col = 1; col <= n-row; col++){
+            cout<<"  ";
+        }
+        for (col = 1 ; col <= row; col++){
+            cout<< " "<< char(col+64);
+        }
+        cout<< endl;
+    }
+    return 0;
+}
