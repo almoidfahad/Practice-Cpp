e9044de6ca1ff2c6af3f43ad11f7efbf26c4e2b2
Al Moid Fahad <almoidfahad@gmail.com>
2024-01-29 19:28:44 +0600

    pattern type 7/1,2,3,4,5

---
+++ pattern_type-7/pattern-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, row, col;
+    cout<< " Enter any Number : ";
+    cin>> n;
+    
+    for (row = 1; row <= n; row++){
+        for (col = 1 ; col <= n; col++){
+            cout<<" "<< row;
+        }
+        cout<< endl;
+    }
+    return 0;
+}
---
+++ pattern_type-7/pattern-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, row , col;
+    cout<< " Enter Number: " ;
+    cin>> n;
+    
+    for (row = 1; row <= n; row ++){
+        for (col = 1 ; col <= n; col++){
+            cout<< " " << char(col+64);
+        }
+        cout<<endl;
+    }
+    return 0;
+}
