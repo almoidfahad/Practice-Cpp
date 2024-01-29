688b08768b6374d6cf0af8dd753bb40cca967213
Al Moid Fahad <almoidfahad@gmail.com>
2024-01-29 22:37:28 +0600

    pattern type 10/1,2,3,4,5

---
+++ pattern_type-10/pattern-5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, row , col;
+    cout<< " Enter Any Number : ";
+    cin>> n;
+    
+    for(row = n; row >= 1; row -- ){
+        for (col = 1 ; col <= n-row ; col++){
+            cout<< "  "; 
+        }
+        for (col = 1 ; col <= 2*row-1; col ++){
+            cout<< " " << "*"; 
+        }
+        cout<< endl;
+    }
+    return 0;
+}
