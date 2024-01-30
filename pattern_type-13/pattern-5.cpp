061209223a11c51915e4affddd178006919aae53
Al Moid Fahad <almoidfahad@gmail.com>
2024-01-30 09:44:37 +0600

    pattern type 13/1,2,3,4,5

---
+++ pattern_type-13/pattern-5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, row, col;
+    cout<<" Enter Number: ";
+    cin>>n;
+    
+    for (row = n; row >= 1; row-- ){
+        for (col = 1; col <= n-row; col ++){
+            cout<<  " ";
+        }
+        for (col = 1; col <= row; col ++){
+            cout<< "*"<< " ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
